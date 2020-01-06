37d35a015f7632a57798c9640351c8cdb4468011
cyl <jhsyydmz@126.com>
2020-01-06 11:57:24 +0800

    Create main.m

---
+++ animation/animation/main.m
@@ -0,0 +1,19 @@
+//
+//  main.m
+//  animation
+//
+//  Created by cyl on 2020/1/6.
+//  Copyright © 2020 cyl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    NSString * appDelegateClassName;
+    @autoreleasepool {
+        // Setup code that might create autoreleased objects goes here.
+        appDelegateClassName = NSStringFromClass([AppDelegate class]);
+    }
+    return UIApplicationMain(argc, argv, nil, appDelegateClassName);
+}
