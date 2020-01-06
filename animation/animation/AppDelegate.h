256d33f4c4d6c3d677bafae0f00b34b811db52ee
cyl <jhsyydmz@126.com>
2020-01-06 11:56:12 +0800

    Create AppDelegate.h

---
+++ animation/animation/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  animation
+//
+//  Created by cyl on 2020/1/6.
+//  Copyright © 2020 cyl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
