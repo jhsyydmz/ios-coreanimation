3bcaccd05d7e73b4bfcd5af055b48004abc9d855
cyl <jhsyydmz@126.com>
2020-01-06 11:57:21 +0800

    Create SceneDelegate.h

---
+++ animation/animation/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  animation
+//
+//  Created by cyl on 2020/1/6.
+//  Copyright © 2020 cyl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
