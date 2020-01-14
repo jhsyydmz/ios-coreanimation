2087960086c8e6df3db9fe550a0df48842f8852c
cyl <jhsyydmz@126.com>
2020-01-14 11:10:32 +0800

    master

--- animationSwift/animationSwift/第11章/BarChartView.swift
+++ animationSwift/animationSwift/第11章/BarChartView.swift
@@ -17,5 +17,51 @@ class BarChartView: UIView {
         // Drawing code
     }
     */
-
+    
+    var chartLine = CAShapeLayer()
+    var pathAnimation = CABasicAnimation()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.backgroundColor = .white
+        self.clipsToBounds = true
+        chartLine.lineCap = .square
+        chartLine.lineJoin = .round
+        chartLine.fillColor = UIColor.gray.cgColor
+        chartLine.lineWidth = 30.0
+        chartLine.strokeEnd = 0.0
+        self.layer.addSublayer(chartLine)
+        drawLineChart()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func drawLineChart() -> Void {
+        chartLine.strokeEnd = 1.0
+        chartLine.add(pathAnimation, forKey: nil)
+        
+    }
+    override func draw(_ rect: CGRect) {
+        let line = UIBezierPath()
+        line.lineWidth = 30.0
+        line.lineCapStyle = .square
+        line.lineJoinStyle = .round
+        for i in 0...4 {
+            let x = CGFloat(60+70*i)
+            let y = CGFloat(100+20*i)
+            line.move(to: CGPoint(x: x, y: 215))
+            line.addLine(to: CGPoint(x: x, y: y))
+            
+        }
+        chartLine.path = line.cgPath
+        chartLine.strokeColor = PNGreen.cgColor
+        pathAnimation.keyPath = "strokeEnd"
+        pathAnimation.timingFunction = CAMediaTimingFunction(name: .linear)
+        pathAnimation.fromValue = 0.0
+        pathAnimation.toValue = 1.0
+        pathAnimation.autoreverses = false
+        pathAnimation.duration = 1.0
+    }
 }
--- animationSwift/animationSwift/第11章/EleventhPartOne.swift
+++ animationSwift/animationSwift/第11章/EleventhPartOne.swift
@@ -9,7 +9,9 @@
 import UIKit
 
 class EleventhPartOne: UIViewController {
-
+    
+    var barchar : BarChartView?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -20,13 +22,38 @@ class EleventhPartOne: UIViewController {
 //        self.view.addSubview(bezier)
         
         //2、
-        let linechart = LineChartView.init(frame: CGRect(x: 0, y:0, width: self.view.bounds.width, height: self.view.bounds.height/2.0))
-        self.view.addSubview(linechart)
+//        let linechart = LineChartView.init(frame: CGRect(x: 0, y:0, width: self.view.bounds.width, height: self.view.bounds.height/2.0))
+//        self.view.addSubview(linechart)
         
         //3、
+        barchar = BarChartView.init(frame: CGRect(x: 0, y: 0, width: self.view.bounds.width, height: self.view.bounds.height))
+        self.view.addSubview(barchar!)
+        addDrawChartButton()
+        addAxes()
     }
     
-
+    func addAxes() -> Void {
+        for i in 0...5 {
+            let yAxesTitle = "SEP"+"\(i)"
+            let yAxesLabel = UILabel.init(frame: CGRect(x: 70+(CGFloat(i)-1)*60, y: 400, width: 50, height: 20))
+            yAxesLabel.text = yAxesTitle
+            self.view.addSubview((yAxesLabel))
+            
+        }
+    }
+    func addDrawChartButton() -> Void {
+        let bt_line = UIButton()
+        bt_line.frame = CGRect(x: self.view.frame.width-100, y: 20, width: 100, height: 50)
+        bt_line.setTitle("Line chart", for: .normal)
+        bt_line.setTitleColor(PNGreen, for: .normal)
+        bt_line.addTarget(self, action: #selector(drawChartMethod), for: .touchUpInside)
+        self.view.addSubview(bt_line)
+        
+    }
+    
+    @objc func drawChartMethod() -> Void {
+        barchar?.drawLineChart()
+    }
     /*
     // MARK: - Navigation
 
