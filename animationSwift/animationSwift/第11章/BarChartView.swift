114d19c93bca171467b2c4b37f12934475dc7bfd
cyl <jhsyydmz@126.com>
2020-01-14 09:29:45 +0800

    master

---
+++ animationSwift/animationSwift/第11章/BarChartView.swift
@@ -0,0 +1,21 @@
+//
+//  BarChartView.swift
+//  animationSwift
+//
+//  Created by cyl on 2020/1/14.
+//  Copyright © 2020 cyl. All rights reserved.
+//
+
+import UIKit
+
+class BarChartView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
